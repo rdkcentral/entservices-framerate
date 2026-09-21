437a4d2982c2791b013e0f086b933c62b9670301
SatheeshkumarG15 <satheeshkumar00150@gmail.com>
2026-09-21 18:36:35 +0530

    Removed USE_THUNDER_R4 in Thunder 5 branch

--- plugin/FrameRateImplementation.h
+++ plugin/FrameRateImplementation.h
@@ -84,13 +84,13 @@ namespace Thunder {
                         }
 
                     public:
-                        static Core::ProxyType<Core::IDispatch> Create(FrameRateImplementation* framerateImplementation, Event event, JsonValue params) {
-#ifndef USE_THUNDER_R4
-                           return (Core::proxy_cast<Core::IDispatch>(Core::ProxyType<Job>::Create(framerateImplementation, event, params)));
-#else
-                           return (Core::ProxyType<Core::IDispatch>(Core::ProxyType<Job>::Create(framerateImplementation, event, params)));
-#endif
-                        }
+                      static Core::ProxyType<Core::IDispatch>
+                      Create(FrameRateImplementation *framerateImplementation,
+                             Event event, JsonValue params) {
+                        return (Core::ProxyType<Core::IDispatch>(
+                            Core::ProxyType<Job>::Create(
+                                framerateImplementation, event, params)));
+                      }
 
                         virtual void Dispatch() {
                             _framerateImplementation->DispatchDSMGRDisplayFramerateChangeEvent(_event, _params);
